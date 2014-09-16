51e0ecebcaf5aa5b47b8e91c012c4a6bca652d69
Wferr <info@wferr.com>
2014-09-16 06:56:30 -0700

    MORRR BACKGROUNDS

--- js/backgroundchanger.js
+++ js/backgroundchanger.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-	$('body').css('background-image', 'url(bg/'+((Math.random() * 7) | 0)+'.jpg)')
+	$('body').css('background-image', 'url(bg/'+((Math.random() * 17) | 0)+'.jpg)')
 });
