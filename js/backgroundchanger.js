9a70c679a656e55ccba57381ddf01320f5feb1dd
Wferr <info@wferr.com>
2014-09-17 07:53:10 -0700

    Verry much in progress push

--- js/backgroundchanger.js
+++ js/backgroundchanger.js
@@ -1,3 +1,4 @@
 $(document).ready(function () {
 	$('body').css('background-image', 'url(bg/'+((Math.random() * 17) | 0)+'.jpg)')
+	$('#projects').css('background-image', 'url(bg/'+((Math.random() * 17) | 0)+'.jpg)')
 });
