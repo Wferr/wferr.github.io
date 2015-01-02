f181d443e80d47459ba2881b49b57be93541d083
Wferr <Wferr@users.noreply.github.com>
2015-01-01 16:31:56 -0800

    Update quotegrabber.js

--- quotes/js/quotegrabber.js
+++ quotes/js/quotegrabber.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
         var lines = quotes.split("\n");
         var randLineNum = Math.floor(Math.random() * lines.length);
         $('h1').html(lines[randLineNum]);
-		$('div').html("<audio autoplay controls='controls'  src='http://tts-api.com/tts.mp3?q=' + lines[randLineNum] + '></audio>"); 
+		$('div').html("<audio autoplay controls='controls'  src="http://tts-api.com/tts.mp3?q=" + lines[randLineNum] + "></audio>"); 
     });
 });
