b496f11e7e62b06175b50ec06a773c901fbf871f
Wferr <Wferr@users.noreply.github.com>
2015-01-01 19:01:46 -0800

    Update quotegrabber.js

--- quotes/js/quotegrabber.js
+++ quotes/js/quotegrabber.js
@@ -1,9 +1,33 @@
 $(document).ready(function () {
     $.get('quotes.txt', function(quotes) {
         var lines = quotes.split("\n");
-        var randLineNum = Math.floor(Math.random() * lines.length);
+        var randLineNum = getNum(lines.length);
         var speach = 'http://tts-api.com/tts.mp3?q='
         $('h1').html(lines[randLineNum]);
 	$('div').html("<audio autoplay controls='controls'  src=" + speach  + encodeURIComponent(lines[randLineNum]) + "'></audio>");
     });
 });
+
+function getQueryParams(qs) {
+    qs = qs.split("+").join(" ");
+
+    var params = {}, tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])]
+            = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
+function getNum(numOfLines){
+	var temp = getQueryParams(document.location.search);
+	var num = Math.floor(Math.random() * numOfLines);
+	if(temp.length > 0){
+		num = temp.quote;
+	}
+	return num;
+}
+		
