30f24feb71902ff52d47f3ccbfd093433b97e240
Wferr <Wferr@users.noreply.github.com>
2015-01-01 16:39:52 -0800

    Update quotegrabber.js

--- quotes/js/quotegrabber.js
+++ quotes/js/quotegrabber.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
     $.get('quotes.txt', function(quotes) {
         var lines = quotes.split("\n");
         var randLineNum = Math.floor(Math.random() * lines.length);
+        var speach = http://tts-api.com/tts.mp3?q=
         $('h1').html(lines[randLineNum]);
-	$('div').html("<audio autoplay controls='controls'  src='http://tts-api.com/tts.mp3?q=' + lines[randLineNum] + '></audio>");
+	$('div').html("<audio autoplay controls='controls'  src=" + speach + lines[randLineNum] + "'></audio>");
     });
 });
