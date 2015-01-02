7fa203d3ea2d934b7eecb936d82cc1ad0067e37f
Dabo Ross <daboross@daboross.net>
2015-01-01 16:40:12 -0800

    Update quotegrabber.js

--- quotes/js/quotegrabber.js
+++ quotes/js/quotegrabber.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $.get('quotes.txt', function(quotes) {
         var lines = quotes.split("\n");
         var randLineNum = Math.floor(Math.random() * lines.length);
-        var speach = http://tts-api.com/tts.mp3?q=
+        var speach = 'http://tts-api.com/tts.mp3?q='
         $('h1').html(lines[randLineNum]);
 	$('div').html("<audio autoplay controls='controls'  src=" + speach + lines[randLineNum] + "'></audio>");
     });
