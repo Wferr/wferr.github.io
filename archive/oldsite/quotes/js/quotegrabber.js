b386a25e085d561af27d9095f53d7b805952df88
Wferr <info@wferr.com>
2015-01-19 14:08:47 -0800

    fixes

---
+++ archive/oldsite/quotes/js/quotegrabber.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $.get('quotes.txt', function(quotes) {
+        var lines = quotes.split("\n");
+        var randLineNum = getNum(lines.length);
+        var speach = 'http://tts-api.com/tts.mp3?q='
+        $('h1').html(lines[randLineNum]);
+	   $('div').html("<audio autoplay controls='controls'  src=" + speach  + encodeURIComponent(lines[randLineNum]) + "'></audio>");
+       $('a').attr("href", "http://wferr.com/quotes/speak?quote=" + randLineNum)
+    });
+});
+
+function getQueryParams(qs) {
+    qs = qs.split("+").join(" ");
+    var total = qs.split("?");
+    var params = {};
+    for(var i = 1; i < total.length; i++){
+        var temp = total[i].split("=");
+        params[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
+    }
+    return params;
+}
+
+function getNum(numOfLines){
+	var temp = getQueryParams(document.location.search);
+	var num = Math.floor(Math.random() * numOfLines);
+	if(temp.quote > 0){
+		num = temp.quote;
+	}
+	return num;
+}
+		
---
+++ archive/oldsite/quotes/linus/js/backgroundchanger.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+	$('body').css('background-image', 'url(bg/'+((Math.random() * 12) | 0)+'.jpg)')
+});
